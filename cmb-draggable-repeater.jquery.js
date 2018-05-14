1a94ed0a463c4b5582c540b18f6f06e00aa9931b
rogerlos <git@web.rogerlos.com>
2018-05-14 09:15:04 -0700

    1.1

--- cmb-draggable-repeater.jquery.js
+++ cmb-draggable-repeater.jquery.js
@@ -7,16 +7,16 @@
  * @link      https://github.com/rogerlos
  * @version   1.0
  */
-(function( $ ) {
+( function ( $ ) {
 
-    $.fn.cmbDraggableRepeater = function( OPT ) {
+    $.fn.cmbDraggableRepeater = function ( OPT ) {
 
-        OPT = typeof( OPT ) === 'undefined' ? {} : ( typeof( OPT ) === 'string' ? { sort: OPT } : OPT );
+        OPT = typeof( OPT ) === 'undefined' ? {} : ( typeof( OPT ) === 'string' ? { sort : OPT } : OPT );
 
         let CFG   = $.extend( {
-                sort: '.cmb-tbody',
-                row: '.cmb-repeat-row',
-                item: '[data-iterator]'
+                sort : '.cmb-tbody',
+                row  : '.cmb-repeat-row',
+                item : '[data-iterator]'
             }, OPT ),
             $SORT = $( this ).find( CFG.sort ),
             THIS  = this;
@@ -27,20 +27,20 @@
             stop : function () {
 
                 // find items, set counter to 0
-                let $items = $SORT.find( CFG.row ).find( CFG.item ),
-                    i = 0,
+                let $items  = $SORT.find( CFG.row ).find( CFG.item ),
+                    i       = 0,
                     swapped = [],
                     EVENT;
 
                 // if there are no items, falls back to using top-level dom elements below $SORT
                 if ( ! $items.length ) $items = $SORT.children();
 
-                $items.each( function() {
+                $items.each( function () {
 
                     let $this = $( this ),
                         idstr = $this.attr( 'id' ).split( '_' ),
-                        id = idstr.pop(),
-                        nm = $this.attr( 'name' );
+                        id    = idstr.pop(),
+                        nm    = $this.attr( 'name' );
 
                     // add the old:new relationship to the event
                     swapped.push( [ id, i ] );
@@ -57,13 +57,15 @@
                     if ( CFG.item === '[data-iterator]' ) $this.attr( 'data-iterator', i ).data( 'iterator', i );
 
                     i ++;
-                });
+                } );
 
-                EVENT = new CustomEvent( 'cmb_drag_sort', { detail: {
-                    0: THIS,
-                    1: this,
-                    swapped: swapped
-                } } );
+                EVENT = new CustomEvent( 'cmb_drag_sort', {
+                    detail : {
+                        0       : THIS,
+                        1       : this,
+                        swapped : swapped
+                    }
+                } );
 
                 $SORT.closest( '.cmb-repeat-table' ).dispatchEvent( EVENT );
             }
@@ -71,4 +73,4 @@
 
         return this;
     };
-}( jQuery ));
+}( jQuery ) );
